ff36a4b1ef22722875a22055f48f80e2e3978479
Agnes <agsawicka@gmail.com>
2022-08-15 08:57:47 -0400

    commit tree de33dfa19e7a8f216abd43ee4b424177d65b6e3e

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, sex) 
+VALUES("Mr. Chocolate", "F"),
+("Rowdy", "M"),
+("Tabitha", "F"),
+("Sergeant Brown", "M"),
+("Melissa", "M"),
+("Grinch", "M"),
+("Wendy", "F"),
+(null, "M");
+
